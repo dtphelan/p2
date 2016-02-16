1e5cb9c12e7aa491faed7d9551755a1a2893738e
dtphelan <dtphelan@gmail.com>
2016-02-16 12:08:52 -0500

    Cleaned files, updated to do with tasks from grading rubric.

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 P2 for DWA-15. Currently a version that covers the project requirements, with more features/styling to come.
 
+## Outside code
+
+[Bootstrap](https://getbootstrap.com/) styles the day.
+
+The random words chosen are scraped from [Paul Noll's website](http://www.paulnoll.com/Books/Clear-English/). I tried to limit the loading time by only choosing one random page to scrape, but that may still slow the page load.
+
 ## To Do
 
 - [ ] Spacing options
 - [ ] Set default capitalization
 - [ ] All styling
 - [ ] Extra features, if inspiration strikes
+- [ ] Record demo
+- [ ] Make the purpose clear
--- logic.php
+++ logic.php
@@ -28,19 +28,18 @@ $html = '';
 
 $urls = Array ('http://www.paulnoll.com/Books/Clear-English/words-01-02-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-03-04-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-05-06-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-07-08-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-09-10-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-11-12-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-13-14-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-15-16-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-17-18-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-19-20-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-21-22-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-23-24-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-25-26-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-27-28-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-29-30-hundred.html');
 
-# Choose a random URL from Paul's website, instead of loading each page. Nicer to Paul, and improves performance of the password generator.
+# Choose a random URL from Paul's website, instead of loading each page (nicer to Paul, and improves performance of the password generator). Then, put the URL's HTML into $html.
 $key = array_rand($urls);
 $chosenUrl = $urls[$key];
-
 $html = $html . file_get_contents($chosenUrl);
 
-# Find everything between <li> and </li> on the chosen page, then put them in the randomWords array
+# Find everything between <li> and </li> on the chosen page, then put them in the $randomWords array
 preg_match_all('|<li>(.*?)</li>|s', $html, $out);
 $randomWords = $out[1];
 
 $randomSymbols = Array ('!','@','#','$','%','^','&','*','~','?','+',':',';','<','>','{','}','.',',');
 
-# Working on implementing more custimization options for users
+# Working on implementing more customization options for users
 $hyphenated = 'no';
 $spaces = '';
 
@@ -50,6 +49,7 @@ for($i = 0; $i < $words; $i++) {
     $key = array_rand($randomWords);
     $chosenWord = $randomWords[$key];
 
+    # Capitalization options
     if($capitalization == 'lowercase') {
         $chosenWord = strtolower($chosenWord);
         $lowerCheck = 'checked';
@@ -81,7 +81,7 @@ if($numbers == 'on') {
 
 }
 
-# Concatenate a symbol to $password
+# Concatenate a symbol from $randomSymbols to $password
 if($symbols == 'on') {
 
     $key = array_rand($randomSymbols);
