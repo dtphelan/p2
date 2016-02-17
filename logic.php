8a47d1c1f8da79236bf3c207df951061cab54281
dtphelan <dtphelan@gmail.com>
2016-02-17 10:37:48 -0500

    Cleaned up logic.php code.

--- logic.php
+++ logic.php
@@ -1,21 +1,5 @@
 <?php
 
-# Define blank variables
-$words = '';
-$numbers = '';
-$symbols = '';
-$symbolCheck = '';
-$numberCheck = '';
-$camelCheck = '';
-$upperCheck = '';
-$lowerCheck = '';
-$capitalization = '';
-$password = '';
-$spacing = '';
-$hyphenatedCheck = '';
-$spacesCheck = '';
-$noSpacingCheck = '';
-
 # Get $_GET variables
 if(isset($_GET['words'])) {
     $words = $_GET['words'];
@@ -48,31 +32,29 @@ if($noSpacingCheck == '') {
 }
 
 # Make sure that $words, $numbers, $symbols, $spacing, and $capitalization are appropriate values
-if(is_numeric($words) !== true) {
+if(is_numeric($words) != true) {
     $password = 'Oh no! ' . $words . ' isn\'t a number. Try entering one (i.e. 4, 7, 2)!';
 }
 
-elseif($numbers !== 'on' AND $numbers !== '') {
+elseif($numbers != 'on' AND $numbers != '') {
     $password = 'Numbers can\'t equal ' . $numbers . '. Try using the form again!';
 }
 
-elseif($symbols !== 'on' AND $symbols !== '') {
+elseif($symbols != 'on' AND $symbols != '') {
     $password = 'Symbols can\'t equal ' . $symbols . '. Try using the form again!';
 }
 
-elseif($spacing !== 'none' AND $spacing !== 'hyphenated' AND $spacing !== 'spaces') {
+elseif($spacing != 'none' AND $spacing != 'hyphenated' AND $spacing != 'spaces') {
     $password = 'Spacing can\'t equal ' . $spacing . '. Try using the form again!';
 }
 
-elseif($capitalization !== 'camelcase' AND $capitalization !== 'uppercase' AND $capitalization !== 'lowercase') {
+elseif($capitalization != 'camelcase' AND $capitalization != 'uppercase' AND $capitalization != 'lowercase') {
     $password = 'Capitalization can\'t equal ' . $capitalization . '. Try using the form again!';
 }
 
 else {
 
     # Scraper function
-    $html = '';
-
     $urls = Array ('http://www.paulnoll.com/Books/Clear-English/words-01-02-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-03-04-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-05-06-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-07-08-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-09-10-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-11-12-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-13-14-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-15-16-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-17-18-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-19-20-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-21-22-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-23-24-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-25-26-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-27-28-hundred.html', 'http://www.paulnoll.com/Books/Clear-English/words-29-30-hundred.html');
 
     # Choose a random URL from Paul's website, instead of loading each page (nicer to Paul, and improves performance of the password generator). Then, put the URL's HTML into $html.
@@ -93,7 +75,7 @@ else {
         $chosenWord = $randomWords[$key];
         $chosenWord = preg_replace('~\W~', '', $chosenWord);
 
-            # Capitalization options
+        # Capitalization options
         if($capitalization == 'lowercase') {
             $chosenWord = strtolower($chosenWord);
             $lowerCheck = 'checked';
@@ -109,6 +91,7 @@ else {
             $camelCheck = 'checked';
         }
 
+        # Spacing options
         if($spacing == 'hyphenated') {
             $chosenWord = $chosenWord . '-';
             $password = $password . $chosenWord;
